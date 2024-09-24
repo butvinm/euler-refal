0a0f29916da60df0abc6c37be7caf1753bdfb081
Mihail Butvin <butvin.mihail@yandex.ru>
2024-09-24 16:04:44 +0300

    Restructurize, solve problem 9

---
+++ solutions/9/9.py
@@ -0,0 +1,12 @@
+def solve() -> int | None:
+    for a in range(0, 333 + 1):
+        for b in range(a + 1, 1000 - a):
+            c = 1000 - b - a
+            if a**2 + b**2 == c**2:
+                return a * b * c
+
+    return None
+
+
+if __name__ == '__main__':
+    print(solve())
